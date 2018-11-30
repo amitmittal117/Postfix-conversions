50e940e1f986904adb4c3ccca97242d86cf57452
amitmittal117 <amitarmittal@gmail.com>
2018-11-30 19:28:18 +0530

    first commit

---
+++ part2.py
@@ -0,0 +1,46 @@
+def evaluate(str_in):
+	tokens = str_in.split(" ")
+	stack = []
+	while len(tokens) > 0:
+		item = tokens.pop(0)
+
+		if item.isdigit():
+			stack.append(int(item))
+
+		elif item == "+":
+			if len(stack) > 1:
+				stack.append(stack.pop() + stack.pop())
+			else:
+				print ("ERROR: Invalid expression. Not enough input.")
+				
+		elif item == "-":
+			if len(stack) > 1:
+				tmp = stack.pop()
+				stack.append(stack.pop() - tmp)
+			else:
+				print ("ERROR: Invalid expression. Not enough input.")
+				
+		elif item == "*":
+			if len(stack) > 1:
+				stack.append(stack.pop() * stack.pop())
+			else:
+				print ("ERROR: Invalid expression. Not enough input.")
+
+		elif item == "/":
+			if len(stack) > 1:
+				tmp = stack.pop()
+				stack.append(stack.pop() / tmp)
+			else:
+				print ("ERROR: Invalid expression. Not enough input.")
+
+		elif item == "^":
+			if len(stack) > 1:
+				tmp = stack.pop()
+				stack.append(pow(stack.pop(), tmp))
+			else:
+				print ("ERROR: Invalid Expression. Not enough input.")           
+
+	return (stack.pop())
+
+
+print(evaluate("1 3 * 125 +"))
